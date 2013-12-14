a783b030f8a062290cabbe05aa8a3484f6b35f7a
Humberto Fraga <xisberto@gmail.com>
2013-12-13 23:30:30 -0300

    Autoloading older days when scrolling history to the past

--- src/net/xisberto/work_schedule/HistoryPageAdapter.java
+++ src/net/xisberto/work_schedule/HistoryPageAdapter.java
@@ -1,34 +1,90 @@
 package net.xisberto.work_schedule;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 
-public class HistoryPageAdapter extends FragmentStatePagerAdapter {
+import com.viewpagerindicator.TabPageIndicator;
 
-	public HistoryPageAdapter(FragmentManager fm) {
+public class HistoryPageAdapter extends FragmentPagerAdapter implements
+		ViewPager.OnPageChangeListener {
+
+	private List<Calendar> days;
+	public TabPageIndicator indicator;
+
+	public HistoryPageAdapter(FragmentManager fm, TabPageIndicator indicator) {
 		super(fm);
+		this.indicator = indicator;
+		days = new ArrayList<Calendar>();
+		for (int i = -4; i <= 0; i++) {
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.DAY_OF_MONTH, i);
+			days.add(cal);
+		}
 	}
 
 	@Override
 	public CharSequence getPageTitle(int position) {
 		DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
-		return dateFormat.format(Calendar.getInstance().getTime());
+		return dateFormat.format(days.get(position).getTime());
 	}
 
 	@Override
 	public Fragment getItem(int index) {
-		Calendar selected_day = Calendar.getInstance();
-		selected_day.add(Calendar.DAY_OF_MONTH, index);
-		return ViewDayFragment.newInstance(selected_day);
+		ViewDayFragment fragment = ViewDayFragment.newInstance(days.get(index));
+		return fragment;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return days.get(position).getTimeInMillis();
+	}
+
+	@Override
+	public int getItemPosition(Object object) {
+		return POSITION_NONE;
 	}
 
 	@Override
 	public int getCount() {
-		return 5;
+		return days.size();
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+		Calendar cal;
+		Log.d("Pager", "position: " + position);
+		switch (position) {
+		case 0:
+			cal = (Calendar) days.get(0).clone();
+			cal.add(Calendar.DAY_OF_MONTH, -1);
+			days.add(0, cal);
+			Log.d("Pager", "size: " + days.size());
+			notifyDataSetChanged();
+			indicator.notifyDataSetChanged();
+			indicator.setCurrentItem(position+1);
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state) {
+
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset,
+			int positionOffsetPixels) {
+
 	}
 
 }
--- src/net/xisberto/work_schedule/ViewHistoryActivity.java
+++ src/net/xisberto/work_schedule/ViewHistoryActivity.java
@@ -1,12 +1,21 @@
 package net.xisberto.work_schedule;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
+import net.xisberto.work_schedule.database.Period;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.widget.DatePicker;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.viewpagerindicator.TabPageIndicator;
 
@@ -26,6 +35,8 @@ public class ViewHistoryActivity extends SherlockFragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_history);
 
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 		if ((savedInstanceState != null && savedInstanceState
 				.containsKey(CURRENT_DAY))) {
 			current_day = (Calendar) savedInstanceState
@@ -34,15 +45,17 @@ public class ViewHistoryActivity extends SherlockFragmentActivity {
 			current_day = Calendar.getInstance();
 		}
 
-		pager_adapter = new HistoryPageAdapter(getSupportFragmentManager());
 		view_pager = (ViewPager) findViewById(R.id.pager);
+		TabPageIndicator pager_indicator = (TabPageIndicator) findViewById(R.id.pager_indicator);
+
+		pager_adapter = new HistoryPageAdapter(getSupportFragmentManager(),
+				pager_indicator);
 		view_pager.setAdapter(pager_adapter);
 
-		TabPageIndicator pager_indicator = (TabPageIndicator) findViewById(R.id.pager_indicator);
 		pager_indicator.setViewPager(view_pager);
+		pager_indicator.setCurrentItem(4);
+		pager_indicator.setOnPageChangeListener(pager_adapter);
 
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		setupTabs();
 
 	}
@@ -54,12 +67,74 @@ public class ViewHistoryActivity extends SherlockFragmentActivity {
 	}
 
 	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.activity_history, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (!BuildConfig.DEBUG) {
+			menu.findItem(R.id.menu_fake_data).setVisible(false);
+		}
+		return true;
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == android.R.id.home) {
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
+		switch (item.getItemId()) {
+		case R.id.menu_fake_data:
+			DialogFragment dialog = new SelectDate();
+			dialog.show(getSupportFragmentManager(), "select_date");
+			break;
+
+		default:
+			break;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 
+	public static class SelectDate extends DialogFragment implements
+			OnDateSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			Calendar now = Calendar.getInstance();
+			int year = now.get(Calendar.YEAR);
+			int month = now.get(Calendar.MONTH);
+			int day = now.get(Calendar.DAY_OF_MONTH);
+
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			Calendar date = Calendar.getInstance();
+			date.set(Calendar.YEAR, year);
+			date.set(Calendar.MONTH, monthOfYear);
+			date.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+			while (date.before(Calendar.getInstance())) {
+				Log.d("SelectDate",
+						"Adding date "
+								+ DateFormat.getDateInstance(DateFormat.SHORT)
+										.format(date.getTime()));
+				if ((date.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+						|| (date.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)) {
+					Log.d("SelectDate", "Skipping weekends");
+				} else {
+					for (int id : Period.ids) {
+						Period period = new Period(id, date);
+						period.persist(getActivity());
+					}
+				}
+				date.add(Calendar.DAY_OF_MONTH, 1);
+			}
+		}
+	}
 }
